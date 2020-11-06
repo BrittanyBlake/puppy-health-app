c93b7478cef81e308b8c884e382f432131629fc0
Brittany Blake <brittany.blake49@gmail.com>
2020-11-06 20:49:47 +0100

    add user reducer

--- src/redux/reducers/RootReducer.js
+++ src/redux/reducers/RootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
-const RootReducer = combineReducers({});
+const RootReducer = combineReducers({
+  currentUser: userReducer,
+});
 
 export default RootReducer;
---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  currentUser: {},
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "LOGIN_USER":
+      return { ...state, currentUser: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
