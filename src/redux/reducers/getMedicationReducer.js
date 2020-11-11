8399105da549d15360a77de1f7f83a15735c623b
Brittany Blake <brittany.blake49@gmail.com>
2020-11-11 21:14:56 +0100

    add medication reducer

---
+++ src/redux/reducers/getMedicationReducer.js
@@ -0,0 +1,10 @@
+const getMedicationsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "GET_MEDICATIONS":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default getMedicationsReducer;
