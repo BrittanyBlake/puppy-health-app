2c8bb8c93982210c823c493a32373cd1280f5a3b
Brittany Blake <brittany.blake49@gmail.com>
2020-11-11 20:58:28 +0100

    fix linter errors

--- src/redux/reducers/getGeneralHealthsReducer.js
+++ src/redux/reducers/getGeneralHealthsReducer.js
@@ -1,11 +1,10 @@
 const getGeneralHealthsReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'GET_GENERAL_HEALTHS':
-            return action.payload
-        default: 
-        return state
-    }
-}
-
+  switch (action.type) {
+    case 'GET_GENERAL_HEALTHS':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export default getGeneralHealthsReducer;
