da939b9d78a41197f551324d2b199a55c9ebb595
Brittany Blake <brittany.blake49@gmail.com>
2020-11-11 21:12:24 +0100

    get info for general health from api

--- src/components/general/General.js
+++ src/components/general/General.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PuppyHealthApi from '../../api/healthTracker';
 import Navbar from '../navbar/Navbar';
 import BottomNav from '../bottomNav/BottomNav';
 
-function General() {
+const General = () => {
+  const dispatch = useDispatch();
+  const allGeneralHealths = useSelector(state => state.generalHealths);
+  console.log('health state', allGeneralHealths);
+
+  useEffect(() => {
+    const getGeneralHealth = () => {
+      console.log('hi');
+      dispatch(PuppyHealthApi.getGeneralHealths());
+    };
+    getGeneralHealth();
+  }, [dispatch]);
+
+  if (!allGeneralHealths) {
+    console.log('nada');
+    return null;
+  }
+
   return (
-    <div>
-      <Navbar />
-      <h1> GENERAL PAGE</h1>
-      <BottomNav />
-    </div>
+    console.log('generals', allGeneralHealths),
+    (
+      <div>
+        <Navbar />
+        <h1> GENERAL PAGE</h1>
+        <div>
+          {' '}
+          { allGeneralHealths.map(health => (
+            <div key={health.id}>
+              <p>
+                Date:
+                {health.date}
+              </p>
+              <p>
+                Weight:
+                {health.weight}
+              </p>
+              <p>
+                energy level:
+                {health.energy_level}
+              </p>
+              <p>
+                hunger level:
+                {health.hunger_level}
+              </p>
+              <p>
+                Extra:
+                {health.extra}
+              </p>
+            </div>
+          )) }
+        </div>
+        <BottomNav />
+      </div>
+    )
   );
-}
+};
 
 export default General;
