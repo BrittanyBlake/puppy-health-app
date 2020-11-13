fb4be9438a68d6d7f20816edd168fc198f66ae2e
Brittany Blake <brittany.blake49@gmail.com>
2020-11-13 11:15:00 +0100

    add food details page

--- src/components/food/FoodDetails.js
+++ src/components/food/FoodDetails.js
@@ -3,9 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 // import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 // import PropTypes from 'prop-types';
+import moment from 'moment';
 import PuppyHealthApi from '../../api/healthTracker';
 import Navbar from '../navbar/Navbar';
 import BottomNav from '../bottomNav/BottomNav';
+// import petfood from '../../assets/images/petfood.png';
+import { ReactComponent as FoodImg } from '../../assets/images/pet-food.svg';
 
 const FoodDetails = () => {
   const { foodDetailsIndex } = useParams();
@@ -40,10 +43,29 @@ const FoodDetails = () => {
     (
       <div>
         <Navbar />
+        {/* <img src={petfood} alt="food" style={{ fill: "#97E493" }} /> */}
+        <FoodImg style={{ fill: '#42B5E8' }} height={200} width={200} />
         <h1>
           {' '}
           {formatDate(getFoodDetailsId.date)}
         </h1>
+        <p>BRAND</p>
+        <p>
+          {getFoodDetailsId.brand}
+          {' '}
+        </p>
+        <p>AMOUNT</p>
+        <p>
+          {getFoodDetailsId.amount}
+          {' '}
+          grams
+          {' '}
+        </p>
+        <p>TIME</p>
+        <p>
+          {moment(getFoodDetailsId.time).format('LT')}
+          {' '}
+        </p>
 
         {/* <div>
         {' '}
