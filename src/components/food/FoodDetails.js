e0e6e796d78d797ab4f7ddc8166804b1da1a8904
Brittany Blake <brittany.blake49@gmail.com>
2020-11-13 18:11:33 +0100

    style food details page

--- src/components/food/FoodDetails.js
+++ src/components/food/FoodDetails.js
@@ -4,10 +4,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import moment from 'moment';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import PuppyHealthApi from '../../api/healthTracker';
 import Navbar from '../navbar/Navbar';
 import BottomNav from '../bottomNav/BottomNav';
+import useStyles from './FoodDetail.styles';
 // import petfood from '../../assets/images/petfood.png';
+
 import { ReactComponent as FoodImg } from '../../assets/images/pet-food.svg';
 
 const FoodDetails = () => {
@@ -15,6 +20,7 @@ const FoodDetails = () => {
   const dispatch = useDispatch();
   const getFoodDetailsId = useSelector(state => state.food);
   const formatDate = datetime => new Date(datetime).toDateString();
+  const classes = useStyles();
 
   useEffect(() => {
     const foodDetailsId = () => {
@@ -43,53 +49,46 @@ const FoodDetails = () => {
     (
       <div>
         <Navbar />
-        {/* <img src={petfood} alt="food" style={{ fill: "#97E493" }} /> */}
-        <FoodImg style={{ fill: '#42B5E8' }} height={200} width={200} />
-        <h1>
+        <h1 style={{ color: 'GrayText', fontFamily: 'Helvetica Neue' }}>
           {' '}
           {formatDate(getFoodDetailsId.date)}
         </h1>
-        <p>BRAND</p>
-        <p>
-          {getFoodDetailsId.brand}
-          {' '}
-        </p>
-        <p>AMOUNT</p>
-        <p>
-          {getFoodDetailsId.amount}
-          {' '}
-          grams
-          {' '}
-        </p>
-        <p>TIME</p>
-        <p>
-          {moment(getFoodDetailsId.time).format('LT')}
-          {' '}
-        </p>
 
-        {/* <div>
-        {' '}
-        {allFoods.map(food => (
-          <div key={food.id}>
-            <p>
-              Brand:
-              {food.brand}
-            </p>
-            <p>
-              Amount:
-              {food.amount}
-            </p>
-            <p>
-              time:
-              {food.time}
-            </p>
-            <p>
-              date:
-              {food.date}
-            </p>
-          </div>
-        ))}
-        </div> */}
+        <Container maxWidth="sm">
+          <Paper className={classes.paper}>
+            <Grid container>
+              <Grid container item xs={6}>
+                <FoodImg style={{ fill: '#42B5E8' }} height={200} width={200} />
+              </Grid>
+              <Grid container item xs={6}>
+                <Grid contianer item xs={12}>
+                  <small>BRAND</small>
+                  <p>
+                    {getFoodDetailsId.brand}
+                    {' '}
+                  </p>
+                </Grid>
+                <Grid contianer item xs={12}>
+                  <small>AMOUNT</small>
+                  <p>
+                    {getFoodDetailsId.amount}
+                    {' '}
+                    grams
+                    {' '}
+                  </p>
+                </Grid>
+                <Grid contianer item xs={12}>
+                  <small>TIME</small>
+                  <p>
+                    {moment(getFoodDetailsId.time).format('LT')}
+                    {' '}
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Container>
+
         <BottomNav addLink="/addfood" trackLink="/food" />
       </div>
     )
