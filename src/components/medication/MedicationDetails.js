c3f3018fa0ae7d140f6cbf64d36e50d071f8705e
Brittany Blake <brittany.blake49@gmail.com>
2020-11-13 18:24:00 +0100

    add medication details styles

--- src/components/medication/MedicationDetails.js
+++ src/components/medication/MedicationDetails.js
@@ -4,13 +4,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import moment from 'moment';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import PuppyHealthApi from '../../api/healthTracker';
 import Navbar from '../navbar/Navbar';
 import BottomNav from '../bottomNav/BottomNav';
+import useStyles from './MedicationDetail.styles';
 // import petfood from '../../assets/images/petfood.png';
 import { ReactComponent as Medicine } from '../../assets/images/medicine.svg';
 
 const MedicationDetails = () => {
+  const classes = useStyles();
   const { medicationDetailsIndex } = useParams();
   const dispatch = useDispatch();
   const getMedicationDetailsId = useSelector(state => state.medications);
@@ -23,16 +28,6 @@ const MedicationDetails = () => {
     medicationDetailsId();
   }, []);
 
-  // dispatch(PuppyHealthApi.getMedicationsId());
-
-  // const foodId = computedMatch.params.ood;
-
-  // const foodState = useSelector(state => state.food);
-
-  // React.useEffect(() => {
-  //   dispatch(PuppyHealthApi.getMedicationsId(foodId));
-  // }, []);
-
   if (!getMedicationDetailsId) {
     console.log('nada');
     return null;
@@ -43,58 +38,44 @@ const MedicationDetails = () => {
     (
       <div>
         <Navbar />
-        {/* <img src={petfood} alt="food" style={{ fill: "#97E493" }} /> */}
-        <Medicine style={{ fill: '#ED4631' }} height={200} width={200} />
-        <h1>
+
+        <h1 style={{ color: 'GrayText', fontFamily: 'Helvetica Neue' }}>
           {' '}
           {formatDate(getMedicationDetailsId.date)}
         </h1>
-        <p>DOSAGE</p>
-        <p>
-          {getMedicationDetailsId.dosage}
-          {' '}
-        </p>
-        <p>USE</p>
-        <p>
-          {getMedicationDetailsId.use}
-          {' '}
-        </p>
-        <p>NAME</p>
-        <p>
-          {getMedicationDetailsId.name}
-          {' '}
-          grams
-          {' '}
-        </p>
-        <p>TIME</p>
-        <p>
-          {moment(getMedicationDetailsId.time).format('LT')}
-          {' '}
-        </p>
 
-        {/* <div>
-        {' '}
-        {allFoods.map(food => (
-          <div key={food.id}>
-            <p>
-              Brand:
-              {food.brand}
-            </p>
-            <p>
-              Amount:
-              {food.amount}
-            </p>
-            <p>
-              time:
-              {food.time}
-            </p>
-            <p>
-              date:
-              {food.date}
-            </p>
-          </div>
-        ))}
-        </div> */}
+        <Container maxWidth="sm">
+          <Paper className={classes.paper}>
+            <Grid container>
+              <Grid container item xs={6}>
+                <Medicine
+                  style={{ fill: '#ED4631' }}
+                  height={200}
+                  width={200}
+                />
+              </Grid>
+              <Grid container item xs={6}>
+                <Grid contianer item xs={12}>
+                  <small>DOSAGE</small>
+                  <p>{getMedicationDetailsId.dosage}</p>
+                </Grid>
+                <Grid contianer item xs={12}>
+                  <small>USE</small>
+                  <p>{getMedicationDetailsId.use}</p>
+                </Grid>
+                <Grid contianer item xs={12}>
+                  <small>MEDICATION NAME</small>
+                  <p>{getMedicationDetailsId.name}</p>
+                </Grid>
+                <Grid contianer item xs={12}>
+                  <small>TIME</small>
+                  <p>{moment(getMedicationDetailsId.time).format('LT')}</p>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Container>
+
         <BottomNav addLink="/addmedication" trackLink="/medication" />
       </div>
     )
