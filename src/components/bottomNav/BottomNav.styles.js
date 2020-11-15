0877f58c24a198c8b1365be416f3515be3692552
Brittany Blake <brittany.blake49@gmail.com>
2020-11-15 01:06:53 +0100

    add top margin to footer

--- src/components/bottomNav/BottomNav.styles.js
+++ src/components/bottomNav/BottomNav.styles.js
@@ -1,12 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
     position: 'fixed',
     bottom: 0,
     backgroundColor: '#5B8092',
+    marginTop: theme.spacing(4),
   },
-});
+}));
 
 export default useStyles;
